609d2b3563d08eefdb62f3c685386938e7997ec7
OhBlockByJames <jasonwalkman8@gmail.com>
2022-05-06 18:27:45 +0800

    1st commit - Hello world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const styleArgument = { fontSize: '100px', color: 'blue' };
+const showOne = false;
+const userName = "Jackson";
+const tester =(userName)=> {
+  return( 
+      <div>
+          <button> who? </button>
+          <h1> {userName} </h1>
+      </div>
+  );
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+  <h1 className = "title" style = { styleArgument }> Hello world!</h1>
+  <h1> { (showOne===true) ? 1 : 0 } </h1>
+  <h3>{tester(userName)}</h3>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
