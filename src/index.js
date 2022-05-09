97148d5653504f71491abbee2db853bcf160dbf8
OhBlockByJames <jasonwalkman8@gmail.com>
2022-05-09 16:11:43 +0800

    2th commit - React props

--- src/index.js
+++ src/index.js
@@ -2,25 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const styleArgument = { fontSize: '100px', color: 'blue' };
-const showOne = false;
-const userName = "Jackson";
-const tester =(userName)=> {
-  return( 
+
+
+function Progress(props){
+  const barWidth=40+parseInt(props.Num);
+  return(
       <div>
-          <button> who? </button>
-          <h1> {userName} </h1>
+      <h5>{props.Num} {props.barName}</h5>
+      <div className="progress-back" style={{backgroundColor:"#8E8E8E",width:"200px",height:"7px",borderRadius:"10px"}}>
+        <div className="progress-bar" style={{backgroundColor:"#EA0000",width:barWidth+"%",height:"100%",borderRadius:"10px"}}></div>
       </div>
+    </div>
   );
 }
+
+var number = 10;
+const addNumber=()=>{
+  number = number + 10;
+  document.getElementById("text").innerHTML=number;
+  if (number>100){
+    number = 0;
+    document.getElementById("text").innerHTML=number;
+  }
+}
+
+const drop=()=>{
+  number = number - 10;
+  document.getElementById("text").innerHTML=number;
+  if (number<0){
+    number = 0;
+    document.getElementById("text").innerHTML=number;
+  }
+}
+
 root.render(
-  <div>
-  <h1 className = "title" style = { styleArgument }> Hello world!</h1>
-  <h1> { (showOne===true) ? 1 : 0 } </h1>
-  <h3>{tester(userName)}</h3>
-  </div>
+  <React.Fragment>
+    <App name="+" handleClick={addNumber}/>
+    <App name="-" handleClick={drop}/>
+    <div id="text">{number}</div>
+    <Progress barName="bar1" Num="50"/>
+  </React.Fragment>
 );
 
 reportWebVitals();
